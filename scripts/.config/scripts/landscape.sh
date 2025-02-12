e458e9530d5a0bd08534043c4ac062c3c1067c48
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2025-02-12 17:26:24 +0300

    configure rotate scripts

---
+++ scripts/.config/scripts/landscape.sh
@@ -0,0 +1,4 @@
+hyprctl keyword monitor ,preferred,auto,auto,transform,0
+hyprctl keyword input:touchdevice:transform 0
+hyprctl keyword input:tablet:transform 0
+hyprctl hyprpaper reload ,"~/.config/wallpapers/space-1.jpg"
---
+++ scripts/.config/scripts/portrait.sh
@@ -0,0 +1,6 @@
+hyprctl keyword monitor ,preferred,auto,auto,transform,1
+hyprctl keyword input:touchdevice:transform 1
+hyprctl keyword input:tablet:transform 1
+hyprctl hyprpaper reload ,"~/.config/wallpapers/space-1.jpg"
+
+

8c2390bca5b1a0c46693b52abcf9a2b46067f29d
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2025-02-23 15:21:56 +0300

    change some keybindings.lua

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -59,16 +59,16 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 --Movement
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<leader>wa", "<C-w>h")
+vim.keymap.set("n", "<leader>wd", "<C-w>j")
+vim.keymap.set("n", "<leader>ws", "<C-w>k")
+vim.keymap.set("n", "<leader>wf", "<C-w>l")
 
 --Resize
-vim.keymap.set("n", "<C-Up>", ":resize -1<CR>")
-vim.keymap.set("n", "<C-Down>", ":resize +1<CR>")
-vim.keymap.set("n", "<C-Left>", ":vertical resize -1<CR>")
-vim.keymap.set("n", "<C-Right>", ":vertical resize +1<CR>")
+vim.keymap.set("n", "<leader>rs", ":resize -1<CR>")
+vim.keymap.set("n", "<leader>rd", ":resize +1<CR>")
+vim.keymap.set("n", "<leader>ra", ":vertical resize -1<CR>")
+vim.keymap.set("n", "<leader>rf", ":vertical resize +1<CR>")
 
 --Move Code
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -80,7 +80,7 @@ vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("n", "<TAB>", ":bn<CR>")
 vim.keymap.set("n", "<S-TAB>", ":bp<CR>")
 vim.keymap.set("n", "<leader>bd", ":bd<CR>") -- from Doom Emacs
-
+vim.keymap.set("n", "<leader>qb",":bw<CR>")
 --Searching
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")

90cbaa2555ffe0c80d48ea830cf361e6f1834341
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2025-03-04 10:26:36 +0300

    changes the hyprpaper reload wallpaper

--- scripts/.config/scripts/landscape.sh
+++ scripts/.config/scripts/landscape.sh
@@ -1,4 +1,4 @@
 hyprctl keyword monitor ,preferred,auto,2,transform,0
 hyprctl keyword input:touchdevice:transform 0
 hyprctl keyword input:tablet:transform 0
-hyprctl hyprpaper reload ,"~/.config/wallpapers/vader-1.jpg"
+hyprctl hyprpaper reload ,"~/.dotfiles/rofi/.config/rofi/background"
--- scripts/.config/scripts/portrait.sh
+++ scripts/.config/scripts/portrait.sh
@@ -1,5 +1,5 @@
 hyprctl keyword monitor ,preferred,auto,auto,transform,3
 hyprctl keyword input:touchdevice:transform 3
 hyprctl keyword input:tablet:transform 3
-hyprctl hyprpaper reload ,"~/.config/wallpapers/vader-1.jpg"
+hyprctl hyprpaper reload ,"~/.dotfiles/rofi/.config/rofi/background"
 

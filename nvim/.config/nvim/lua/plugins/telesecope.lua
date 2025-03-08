d410bff14054dbc2f9e0ca7e339b972792be389e
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2025-03-09 00:50:42 +0300

    change the find buffers keymap

--- nvim/.config/nvim/lua/plugins/telesecope.lua
+++ nvim/.config/nvim/lua/plugins/telesecope.lua
@@ -17,7 +17,7 @@ return {
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>bb", builtin.buffers, {})
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
       vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
       vim.keymap.set("n", "<leader>fa", "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>")

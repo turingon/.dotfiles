e496b9f9dd172288b40229ac918cb80ac286924f
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2025-05-24 09:27:36 +0300

    change some keybindigs

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -12,48 +12,17 @@ vim.cmd("set nowrap")
 vim.cmd("set relativenumber")
 vim.cmd("set conceallevel=1")
 vim.cmd("let g:tex_conceal='abdmg'")
-vim.cmd("noremap r d")
-vim.cmd("noremap rr dd")
-vim.cmd("noremap a h")
-vim.cmd("noremap s k")
-vim.cmd("noremap d j")
-vim.cmd("noremap f l")
-
---Nord Theme Config
-vim.g.nord_contrast = true
-vim.g.nord_borders = true
-vim.g.nord_disable_background = false
-vim.g.nord_italic = false
-vim.g.nord_uniform_diff_background = true
-vim.g.nord_bold = true
 
 --Latex Inkscape Commands
 vim.cmd("inoremap <C-t> <ESC>:silent exec '!inkscape-figures create '.getline('.')' ./figures/ '<CR><CR>:w<CR> ")
 vim.cmd("nnoremap <C-t> :silent exec '!inkscape-figures edit ./figures/ > /dev/null 2>&1 & '<CR><CR>:redraw!<CR>")
 --Some Useful Insert Mode Bindings
 
---Navigate
-vim.cmd("inoremap <C-a> <Left>")
-vim.cmd("inoremap <C-s> <Up>")
-vim.cmd("inoremap <C-d> <Down>")
-vim.cmd("inoremap <C-f> <Right>")
-vim.cmd("inoremap <C-b> <Esc>bi")
-vim.cmd("inoremap <C-e> <Esc>ea")
-
-
 --Save
-vim.cmd("inoremap <F3> <c-o>:w<cr>")
-
---Undo Redo Not Working
-vim.cmd("inoremap <C-u> <c-o>:u<cr>")
-
+vim.cmd("nnoremap <C-s> <Esc>:wa<cr>")
+vim.cmd("inoremap <C-s> <c-o>:wa<cr>")
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-
---Useful Shortcuts
-vim.keymap.set("n", "<C-s>", ":w<CR>")
-vim.keymap.set("n", "<C-x>", ":wqa<CR>")
-
 
 --UndoTree Persisten History
 vim.opt.swapfile = false
@@ -84,6 +53,7 @@ vim.keymap.set("n", "<TAB>", ":bn<CR>")
 vim.keymap.set("n", "<S-TAB>", ":bp<CR>")
 vim.keymap.set("n", "<leader>bd", ":bd<CR>") -- from Doom Emacs
 vim.keymap.set("n", "<leader>qb",":bw<CR>")
+
 --Searching
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -91,4 +61,4 @@ vim.keymap.set("n", "<C-f>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "G", "Gzz")
-vim.keymap.set("n", "gg", "<leader>b")
+vim.keymap.set("n", "<leader>gg", "<leader>b")
